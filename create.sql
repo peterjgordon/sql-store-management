ce2a30dc8e66038e06f41074eff47200abe340c2
Daniel Penney-Gallagher <penneygallagher@gmail.com>
2017-09-28 14:28:40 +0000

    Database creation script

---
+++ create.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE IF NOT EXISTS amazonia;
+USE amazonia;
+CREATE TABLE IF NOT EXISTS staff(
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(20) NOT NULL,
+    sortcode mediumint NOT NULL,
+    account_number int);
+CREATE TABLE IF NOT EXISTS distributor(
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(20) NOT NULL);
+CREATE TABLE IF NOT EXISTS product_distributor(
+    productID int,
+    distributorID int,
+    PRIMARY KEY(productID, distributorID),
+    FOREIGN KEY (productID) REFERENCES product(id),
+    FOREIGN KEY (distributorID) REFERENCES distributor(id));
+CREATE TABLE IF NOT EXISTS product(
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(20) NOT NULL);
+CREATE TABLE IF NOT EXISTS basket(
+    productID int,
+    customerID int,
+    PRIMARY KEY (productID, customerID),
+    FOREIGN KEY (productID) REFERENCES product(id),
+    FOREIGN KEY (customerID) REFERENCES customer(id));
+CREATE TABLE IF NOT EXISTS customer(
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(20) NOT NULL,
+    address varchar(20) NOT NULL,
+    postcode varchar(8) NOT NULL,
+    card_number bigint NOT NULL,
+    phone_number varchar(16) NOT NULL,
+    FOREIGN KEY (postcode) REFERENCES address(postcode));
+CREATE TABLE IF NOT EXISTS address(
+    postcode varchar(8) NOT NULL,
+    town varchar(20) NOT NULL,
+    county varchar(20) NOT NULL,
+    country varchar(20) NOT NULL);
